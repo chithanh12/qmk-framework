96e677b7bfd4f4d5f2e97d7c17c705ad69958722
nguyenthanh <chithanh12@gmail.com>
2020-01-08 03:02:42 +0700

    update layout

--- keyboards/keebio/quefrency/keymaps/thanhnguyen/keymap.c
+++ keyboards/keebio/quefrency/keymaps/thanhnguyen/keymap.c
@@ -33,25 +33,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_BASE] = LAYOUT(
-    KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
+    KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     TD(T_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, 
-    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_LEFT, KC_DOWN, KC_RGHT
+    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_RCTRL, KC_PGUP, KC_PGDOWN
   ),
 
   [_LAY1] = LAYOUT(
-   KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
+    KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL, 
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     TD(T_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP, 
-    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_LEFT, KC_DOWN, KC_RGHT
+    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_RCTRL, KC_PGUP, KC_PGDOWN
   ),
 
    /* 
    * LOWER
    * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┐     ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   * │  `~ │  1  │  2  │  3  │  4  │  5  │  6  │     │  7  │  8  │  9  │  0  │  -  │  =  │BSLS │ DEL │ 
+   * │  `~ │  F1 │ F2  │ F3  │ F4  │ F5  │ F6  │     │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │BSLS │ DEL │ 
    * ├─────┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┘  ┌──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴──┬──┴─────┤
    * │TAB     │  Q  │  W  │  E  │  R  │  T  │     │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │BSPC    │
    * ├────────┴┬────┴┬────┴┬────┴┬────┴┬────┴┐    └┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴┬────┴────────┤
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_LOWER] = LAYOUT(
-    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_BSPC, KC_DEL,
+    RGB_TOG, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
-    KC_TILD, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______
+    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_RCTRL, KC_PGUP, KC_PGDOWN
   ),
 
    /* 
@@ -87,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_RAISE] = LAYOUT(
-    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_BSPC, KC_DEL,
+    RGB_TOG, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
-    KC_TILD, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______
+    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_RCTRL, KC_PGUP, KC_PGDOWN
   ),
 
    /* 
@@ -111,10 +111,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CONTROL] = LAYOUT(
     KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
-    RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+    RGB_TOG, _______, _______, KC_UP,   _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
-    KC_TILD, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______
+    MO_LOWR, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                             MO_LOWR, KC_RGUI, TG_LOWR, TG_RAIS, KC_RCTRL, KC_PGUP, KC_PGDOWN
   )
 };
 
