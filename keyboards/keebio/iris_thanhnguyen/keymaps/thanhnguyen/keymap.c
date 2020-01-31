7a02ff0b3c7a88e99b144a78149331815046260c
nguyenthanh <chithanh12@gmail.com>
2020-01-31 16:38:50 +0700

    update keymap a bit

--- keyboards/keebio/iris_thanhnguyen/keymaps/thanhnguyen/keymap.c
+++ keyboards/keebio/iris_thanhnguyen/keymaps/thanhnguyen/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐   ┌────────┐   ┌────────┐   ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        TG_LOWR,     TG_RAIS,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤   ├────────┤   ├────────┤   ├────────┼────────┼────────┼────────┼────────┼────────┤
-     TD(T_ESCTAB),KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_SPC,      KC_ENT,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+     TD(T_ESCTAB),KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_SPC,  RGUI_T(KC_ENT),      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤   ├────────┤   ├────────┤   ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        TT_LOWR,     TT_RAIS,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        TT_LOWR,     TT_RAIS,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┤   ├────────┤   ├────────┤   ┌────────┼────────┼────────┼────────┼────────┼────────┤
    TD(T_SHFCAP), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_LGUI,     KC_RALT,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT
   //└────────┴────────┴────────┴────────┴────────┴────────┘   └────────┘   └────────┘   └────────┴────────┴────────┴────────┴────────┴────────┘
