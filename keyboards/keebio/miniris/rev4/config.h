e26f65f2bd578eae6a47e154268e5ecd4c12ab32
nguyenthanh <chithanh12@gmail.com>
2020-04-04 04:18:59 +0700

    fixed somethig

--- keyboards/keebio/miniris/rev4/config.h
+++ keyboards/keebio/miniris/rev4/config.h
@@ -39,8 +39,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   
 #define SPLIT_HAND_PIN D2
 
-#define ENCODERS_PAD_A { F5 }
-#define ENCODERS_PAD_B { F6 }    
+#define ENCODERS_PAD_A { F6 }
+#define ENCODERS_PAD_B { F5 }    
 
 #define ENCODERS_PAD_A_RIGHT { F5 }
 #define ENCODERS_PAD_B_RIGHT { F6 }
