26471f5c9fc1fdd3061c6c206a0f031bf5f226b6
nguyenthanh <chithanh12@gmail.com>
2020-04-28 11:38:49 +0700

    update config for permissive hold

--- keyboards/keebio/iris_thanhnguyen/keymaps/thanhnguyen/config.h
+++ keyboards/keebio/iris_thanhnguyen/keymaps/thanhnguyen/config.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_I2C
 #define EE_HANDS
 #define TAPPING_TERM 175
+#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
