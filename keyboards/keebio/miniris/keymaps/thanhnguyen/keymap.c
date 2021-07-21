78642a8b78505451595aafbe26cc3de6879dc5ec
thanhnguyen <chithanh12@gmail.com>
2021-07-22 00:27:33 +0700

    Fix: tweak abit for linux

--- keyboards/keebio/miniris/keymaps/thanhnguyen/keymap.c
+++ keyboards/keebio/miniris/keymaps/thanhnguyen/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //├───────┼───────┼───────┼───────┼───────┼───────┤                      ├───────┼───────┼───────┼───────┼───────┼───────┤
       ESCLCTL, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_BSPC,
    //├───────┼───────┼───────┼───────┼───────┼───────┼───────┐      ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤ 
-      TD_SHF, RGUI_T(KC_Z), KC_X,    KC_C,   KC_V,   KC_B, TG_LOWR,       TG_RAIS, KC_N,  KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, 
+      TD_SHF, RGUI_T(KC_Z), KC_X, KC_C,   KC_V,   KC_B, TG_LOWR,       TG_RAIS, KC_N,  KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT, 
    //└───────┴───────┴───────┴───┬───┴───┬───┴───┬───┴───┬───┘      └───┬───┴───┬───┴───┬───┴───┬───┼───────┼───────┼───────┤
-                                  KC_LCTL,TT_LOWR, KC_SPC,               KC_ENT, TT_RAIS ,TD_CTRL
+                                  KC_LCTL, TT_LOWR, KC_SPC,               KC_ENT, TT_RAIS ,TD_CTRL
     //                           └───────┴───────┴───────┘              └───────┴───────┴───────┘ 
   ),
 //   ┌─────┬─────┬─────┬─────┬─────┬─────┐                  ┌─────┬─────┬─────┬─────┬─────┬─────┐
